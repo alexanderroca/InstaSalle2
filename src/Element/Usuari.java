1c4800071d35638abb55299be6e0b58a89a7ce95
alexanderroca <alexander.roca@students.salle.url.edu>
2018-12-24 12:50:21 +0100

    FEAT: Classe Usuari

--- src/Element/Usuari.java
+++ src/Element/Usuari.java
@@ -1,5 +1,63 @@
 package Element;
 
-public class Usuari {
+import Element.ElementUsuari.Connection;
+import Element.ElementUsuari.Post;
 
+import java.util.ArrayList;
+
+public class Usuari {
+    private String username;
+    private int followers;
+    private int follows;
+    private double activity;
+    private ArrayList<Connection> connections;
+    private ArrayList<Post> posts;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getFollowers() {
+        return followers;
+    }
+
+    public void setFollowers(int followers) {
+        this.followers = followers;
+    }
+
+    public int getFollows() {
+        return follows;
+    }
+
+    public void setFollows(int follows) {
+        this.follows = follows;
+    }
+
+    public double getActivity() {
+        return activity;
+    }
+
+    public void setActivity(double activity) {
+        this.activity = activity;
+    }
+
+    public ArrayList<Connection> getConnections() {
+        return connections;
+    }
+
+    public void setConnections(ArrayList<Connection> connections) {
+        this.connections = connections;
+    }
+
+    public ArrayList<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(ArrayList<Post> posts) {
+        this.posts = posts;
+    }
 }
