2d6517326663a6d50a24c125eeb12ff7985d6648
alexanderroca <alexander.roca@students.salle.url.edu>
2018-12-24 12:52:40 +0100

    FEAT: Classe Test

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,12 @@
 
 /**
- *Main del programa InstaSalle
+ *Test del programa InstaSalle
  *
  * @author Alexander Roca, Marc Cespedes
  * @version 15/11/2018 - 0.1
  */
 
-public class Main {
+public class Test {
     public static void main(String[] args){
 
     }
