6e9aa5610d6496f033a94d28d807de3adead875e
alexanderroca <alexander.roca@students.salle.url.edu>
2018-12-26 11:39:06 +0100

    FEAT: Classe Solution

---
+++ src/Solution.java
@@ -0,0 +1,12 @@
+import Element.Usuari;
+
+import java.util.ArrayList;
+
+public class Solution {
+
+
+    private ArrayList<String> activitats;
+    private ArrayList<Usuari> usuaris;
+    private int seguent_germa;
+    private int seguent_nivell;
+}
